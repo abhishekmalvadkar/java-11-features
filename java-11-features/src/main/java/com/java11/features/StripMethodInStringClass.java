fa53d207a407f6cb1038564124d6a9ffe48328c7
Abhishek Malvadkar <abhishek@dev.com>
2023-06-07 10:51:10 +0530

    Strip Method In String Class

---
+++ java-11-features/src/main/java/com/java11/features/StripMethodInStringClass.java
@@ -0,0 +1,9 @@
+package com.java11.features;
+
+public class StripMethodInStringClass {
+	
+	public String feature(String content) {
+		return content.strip();
+	}
+
+}
---
+++ java-11-features/src/test/java/com/java11/features/StripMethodInStringClassTest.java
@@ -0,0 +1,41 @@
+package com.java11.features;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class StripMethodInStringClassTest {
+
+	@Test
+	void testFeatureIfStringHasTrailingSpace() {
+		var featureClass = new StripMethodInStringClass();
+		String email = "abc@xyz.com ";
+		String result = featureClass.feature(email);
+		assertThat(result).hasSize(11);
+	}
+	
+	@Test
+	void testFeatureIfStringHasLeadingSpace() {
+		var featureClass = new StripMethodInStringClass();
+		String email = " abc@xyz.com";
+		String result = featureClass.feature(email);
+		assertThat(result).hasSize(11);
+	}
+	
+	@Test
+	void testFeatureIfStringHasTrailingAndLeadingSpace() {
+		var featureClass = new StripMethodInStringClass();
+		String email = " abc@xyz.com ";
+		String result = featureClass.feature(email);
+		assertThat(result).hasSize(11);
+	}
+	
+	@Test
+	void testFeatureIfStringHasOnlyOneSpace() {
+		var featureClass = new StripMethodInStringClass();
+		String email = " ";
+		String result = featureClass.feature(email);
+		assertThat(result).hasSize(0);
+	}
+
+}
